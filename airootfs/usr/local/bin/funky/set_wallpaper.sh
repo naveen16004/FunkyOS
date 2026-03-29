ddfc307cdde86bdd6ca86259c96c35f8ddc3d595
naveen16004 <naveen16004@gmail.com>
2026-03-29 06:33:25 +0000

    feat: add ghost-mode wallpaper logic

---
+++ airootfs/usr/local/bin/funky/set_wallpaper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 🖼️ FunkyOS: Ghost Wallpaper Logic
+
+# Set a solid black background (Best for Stealth/OLED)
+swww img /usr/share/backgrounds/funky_ghost.png || swaybg -c "#000000"
+
+echo "🖼️ Funky Wallpapers Set to Stealth Mode."
