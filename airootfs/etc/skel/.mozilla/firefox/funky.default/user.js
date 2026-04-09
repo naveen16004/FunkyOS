20c28fdf69edf7804b65335cbb8801db8241d170
Naveen16004 <naveen16004@gmail.com>
2026-04-09 20:43:35 +0530

    Hardened Firefox profile and integrated DNSCrypt

---
+++ airootfs/etc/skel/.mozilla/firefox/funky.default/user.js
@@ -0,0 +1,8 @@
+// FunkyOS Ghost Browser Config
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("dom.battery.enabled", false); // Prevent tracking via battery status
+user_pref("browser.fixup.alternate.enabled", false); // Prevent "guess" searching
