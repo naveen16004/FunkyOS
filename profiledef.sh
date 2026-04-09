ea1a0539277f2ede88293c1da97737ceadc7fd71
Naveen16004 <naveen16004@gmail.com>
2026-04-09 22:25:24 +0530

    Added loader.conf and finalized efiboot structure for systemd-boot

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,22 @@
 iso_name="FunkyOS"
 iso_label="FUNKY_GHOST"
-iso_publisher="FunkyOS Dev"
-iso_application="Ghost System"
+iso_publisher="FunkyOS Dev <https://github.com/your-username>"
+iso_application="FunkyOS Universal Ghost System"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux' 'uefi.grub')
+# Added 'uefi-x86_64.systemd-boot' for better compatibility with modern laptops
+# Modern Standard Naming
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '1')
+# This ensures the 'archiso' boot hooks look for the correct label
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/root"]="0:0:700"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_Guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
